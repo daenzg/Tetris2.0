8bfb3f07fe57dd23ffc1539aae50deed5f2b950c
Daniel Furrer <dfurrer8@gmail.com>
2022-11-07 20:15:46 +0100

    add comments

--- Game.java
+++ Game.java
@@ -45,91 +45,91 @@ public class Game extends World
         time++;
         if (Greenfoot.isKeyDown("down") && !dropped && !downHeld)
         {
-            time = gravity;
-            dropped = true;
-            downHeld = true;
+            time = gravity;                                                     //
+            dropped = true;                                                     //Blöcke schneller senken
+            downHeld = true;                                                    //
         }
         else if (!Greenfoot.isKeyDown("down"))
         {
-            dropped = false;
-            downHeld = false;
+            dropped = false;                                                    //Sicherstellen dass Blöcke nicht schneller gesenkt werden
+            downHeld = false;                                                   //
         }
         
         if (Greenfoot.isKeyDown("left") && delay >= 10 && !collideLeft)
         {
-            delay = 0;
-            currentBlocks[0].setLocation(currentBlocks[0].getX() - 1, currentBlocks[0].getY());
-            currentBlocks[1].setLocation(currentBlocks[1].getX() - 1, currentBlocks[1].getY());
-            currentBlocks[2].setLocation(currentBlocks[2].getX() - 1, currentBlocks[2].getY());
-            currentBlocks[3].setLocation(currentBlocks[3].getX() - 1, currentBlocks[3].getY());
+            delay = 0;                                                                              //
+            currentBlocks[0].setLocation(currentBlocks[0].getX() - 1, currentBlocks[0].getY());     //
+            currentBlocks[1].setLocation(currentBlocks[1].getX() - 1, currentBlocks[1].getY());     //Bewegung der Blöcke nach links (X Koordinaten -1)
+            currentBlocks[2].setLocation(currentBlocks[2].getX() - 1, currentBlocks[2].getY());     //
+            currentBlocks[3].setLocation(currentBlocks[3].getX() - 1, currentBlocks[3].getY());     //
         }
         else { delay++; }
         if (Greenfoot.isKeyDown("right") && delay >= 10 && !collideRight)
         {
-            delay = 0;
-            currentBlocks[0].setLocation(currentBlocks[0].getX() + 1, currentBlocks[0].getY());
-            currentBlocks[1].setLocation(currentBlocks[1].getX() + 1, currentBlocks[1].getY());
-            currentBlocks[2].setLocation(currentBlocks[2].getX() + 1, currentBlocks[2].getY());
-            currentBlocks[3].setLocation(currentBlocks[3].getX() + 1, currentBlocks[3].getY());
+            delay = 0;                                                                              //
+            currentBlocks[0].setLocation(currentBlocks[0].getX() + 1, currentBlocks[0].getY());     //
+            currentBlocks[1].setLocation(currentBlocks[1].getX() + 1, currentBlocks[1].getY());     //Bewegung der Blöcke nach links (X Koordinaten +1)
+            currentBlocks[2].setLocation(currentBlocks[2].getX() + 1, currentBlocks[2].getY());     //
+            currentBlocks[3].setLocation(currentBlocks[3].getX() + 1, currentBlocks[3].getY());     //
         }
         else { delay++; }
         
         boolean shapeWillCollide = 
-            (currentShape == BlockType.I && rotation == 90 ||
-            (currentShape == BlockType.J || currentShape == BlockType.L) && (rotation == 90 || rotation == 270) ||
-            (currentShape == BlockType.S || currentShape == BlockType.Z) && (rotation == 90) ||
-            currentShape == BlockType.T && (rotation == 90 || rotation == 270)
-            );
+            (currentShape == BlockType.I && rotation == 90 ||                                                                   //
+            (currentShape == BlockType.J || currentShape == BlockType.L) && (rotation == 90 || rotation == 270) ||              //
+            (currentShape == BlockType.S || currentShape == BlockType.Z) && (rotation == 90) ||                                 //Prüft ob bei Rotierung die Form mir der Wand Kollidiert
+            currentShape == BlockType.T && (rotation == 90 || rotation == 270)                                                  //
+            );                                                                                                                  //
         
-        boolean stopRotateLeft = shapeWillCollide && collideLeft || currentShape == BlockType.I && rotation == 90 && currentBlocks[0].getX() + 1 <= 2;
-        boolean stopRotateRight = shapeWillCollide && collideRight || currentShape == BlockType.I && rotation == 90 && currentBlocks[0].getX() + 1 >= getWidth();
+        boolean stopRotateLeft = shapeWillCollide && collideLeft || currentShape == BlockType.I && rotation == 90 && currentBlocks[0].getX() + 1 <= 2;              // Blockiert drehung wenn sie nicht möglich ist.
+        boolean stopRotateRight = shapeWillCollide && collideRight || currentShape == BlockType.I && rotation == 90 && currentBlocks[0].getX() + 1 >= getWidth();   //
         
-        if (!rotatedLeft && Greenfoot.isKeyDown("a") && !(stopRotateLeft || stopRotateRight) && !collideBottomNoStop)
-        {
-            rotateLeft();
-            rotatedLeft = true;
-        }
-        else if (!Greenfoot.isKeyDown("a"))
-        {
-            rotatedLeft = false;
-        }
-        if (!rotatedRight && Greenfoot.isKeyDown("d") && !(stopRotateRight || stopRotateLeft)&& !collideBottomNoStop)
-        {
-            rotateRight();
-            rotatedRight = true;
-        }
-        else if (!Greenfoot.isKeyDown("d"))
-        {
-            rotatedRight = false;
-        }
+        if (!rotatedLeft && Greenfoot.isKeyDown("a") && !(stopRotateLeft || stopRotateRight) && !collideBottomNoStop)   //
+        {                                                                                                               //
+            rotateLeft();                                                                                               //
+            rotatedLeft = true;                                                                                         //
+        }                                                                                                               // Bewegung des Blockes nach links
+        else if (!Greenfoot.isKeyDown("a"))                                                                             //
+        {                                                                                                               //
+            rotatedLeft = false;                                                                                        //
+        }                                                                                                               //
+        if (!rotatedRight && Greenfoot.isKeyDown("d") && !(stopRotateRight || stopRotateLeft)&& !collideBottomNoStop)   //
+        {                                                                                                               //
+            rotateRight();                                                                                              //
+            rotatedRight = true;                                                                                        //
+        }                                                                                                               // Bewegung des Blockes nach rechts
+        else if (!Greenfoot.isKeyDown("d"))                                                                             //
+        {                                                                                                               //
+            rotatedRight = false;                                                                                       //
+        }                                                                                                               //
         
-        if ((time >= gravity || (dropped && time > drop)) && !collideBottom())
-        {
-            time = 0;
-            currentBlocks[0].setLocation(currentBlocks[0].getX(), currentBlocks[0].getY() + 1);
-            currentBlocks[1].setLocation(currentBlocks[1].getX(), currentBlocks[1].getY() + 1);
-            currentBlocks[2].setLocation(currentBlocks[2].getX(), currentBlocks[2].getY() + 1);
-            currentBlocks[3].setLocation(currentBlocks[3].getX(), currentBlocks[3].getY() + 1);
-        }
+        if ((time >= gravity || (dropped && time > drop)) && !collideBottom())                                          //
+        {                                                                                                               //
+            time = 0;                                                                                                   //
+            currentBlocks[0].setLocation(currentBlocks[0].getX(), currentBlocks[0].getY() + 1);                         //
+            currentBlocks[1].setLocation(currentBlocks[1].getX(), currentBlocks[1].getY() + 1);                         // Bewegung des Blockes nach unten
+            currentBlocks[2].setLocation(currentBlocks[2].getX(), currentBlocks[2].getY() + 1);                         //
+            currentBlocks[3].setLocation(currentBlocks[3].getX(), currentBlocks[3].getY() + 1);                         //
+        }                                                                                                               //
     }
     
     public void newBlocks()
     {
-        currentShape = BlockType.fromInt(Greenfoot.getRandomNumber(BlockType.values().length));
-        dropped = false;
+        currentShape = BlockType.fromInt(Greenfoot.getRandomNumber(BlockType.values().length));                         // Wählt random Zahl und schribt Sie einem Buchstaben in BlockType zu.
+        dropped = false; 
         rotation = 0;
-        Color blockColor = Color.WHITE;
-
-        switch (currentShape) {
+        Color blockColor = Color.WHITE; 
+        
+        switch (currentShape) {                                                                                         // Erstellt Block wenn Bedingung Zutrifft
             case I:
                 blockColor = Color.RED;
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 3, 0);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 4, 0);
+                addObject(currentBlocks[1], 4, 0);                                                                      // erstellt I Block in rot
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 5, 0);
-                currentBlocks[3] = new Block(blockColor);
+                currentBlocks[3] = new Block(blockColor);                                                               
                 addObject(currentBlocks[3], 6, 0);
                 break;
 
@@ -138,7 +138,7 @@ public class Game extends World
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 6, 1);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 4, 0);
+                addObject(currentBlocks[1], 4, 0);                                                                      // erstellt J Block in blau
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 5, 0);
                 currentBlocks[3] = new Block(blockColor);
@@ -150,7 +150,7 @@ public class Game extends World
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 4, 1);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 4, 0);
+                addObject(currentBlocks[1], 4, 0);                                                                      // erstellt L Block in grün
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 5, 0);
                 currentBlocks[3] = new Block(blockColor);
@@ -162,7 +162,7 @@ public class Game extends World
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 4, 0);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 5, 0);
+                addObject(currentBlocks[1], 5, 0);                                                                      // erstellt O Block in türkis
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 4, 1);
                 currentBlocks[3] = new Block(blockColor);
@@ -174,7 +174,7 @@ public class Game extends World
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 5, 0);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 6, 0);
+                addObject(currentBlocks[1], 6, 0);                                                                      // erstellt S Block in rot
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 4, 1);
                 currentBlocks[3] = new Block(blockColor);
@@ -186,7 +186,7 @@ public class Game extends World
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 4, 0);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 5, 0);
+                addObject(currentBlocks[1], 5, 0);                                                                      // erstellt T Block in gelb
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 6, 0);
                 currentBlocks[3] = new Block(blockColor);
@@ -198,7 +198,7 @@ public class Game extends World
                 currentBlocks[0] = new Block(blockColor);
                 addObject(currentBlocks[0], 4, 0);
                 currentBlocks[1] = new Block(blockColor);
-                addObject(currentBlocks[1], 5, 0);
+                addObject(currentBlocks[1], 5, 0);                                                                      // erstellt Z Block in pink
                 currentBlocks[2] = new Block(blockColor);
                 addObject(currentBlocks[2], 5, 1);
                 currentBlocks[3] = new Block(blockColor);
@@ -209,7 +209,7 @@ public class Game extends World
     
     public void checkRows()
     {
-        for (int row = 0; row < rows.length; row++)
+        for (int row = 0; row < rows.length; row++) //
         {
             if (rows[row] == 10)
             {
@@ -267,7 +267,7 @@ public class Game extends World
         return false;
     }
     
-    public boolean collideLeft()
+    public boolean collideLeft()                                                
     {
         for (Block b : currentBlocks)
         {
@@ -308,7 +308,7 @@ public class Game extends World
                 if (rotation == 0)
                 {
                     currentBlocks[0].setLocation(currentBlocks[2].getX(), currentBlocks[2].getY() - 2);
-                    currentBlocks[1].setLocation(currentBlocks[2].getX(), currentBlocks[2].getY() - 1);
+                    currentBlocks[1].setLocation(currentBlocks[2].getX(), currentBlocks[2].getY() - 1);             
                     currentBlocks[3].setLocation(currentBlocks[2].getX(), currentBlocks[2].getY() + 1);
                     rotation = 90;
                 }
